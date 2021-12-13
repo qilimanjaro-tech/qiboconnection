974b7f00a55f6845e3b83e85fe19e8d2af4f42a5
Albert Solana <iamtxena@gmail.com>
2021-12-13 16:43:24 +0400

    test: new user example test

---
+++ examples/user_example.py
@@ -0,0 +1,34 @@
+
+from qibo.core.circuit import Circuit
+from qibo import gates
+from qiboconnection.api import API
+from qiboconnection.connection import ConnectionConfiguration
+import json
+import os
+
+
+def user_example() -> None:
+
+    qibo_api = API(configuration=ConnectionConfiguration({
+        "user_id": 4,
+        "username": "qili-test-1",
+        "api_key": "1e64261c-4e51-440b-8ae2-74eb36255a4b",
+    }))
+
+    qibo_api = API()
+
+    devices = qibo_api.list_devices()
+    print(devices)
+    qibo_api.select_device_id(device_id=1)
+
+    circuit = Circuit(1)
+    circuit.add(gates.X(0))
+
+    job_id = qibo_api.execute(circuit=circuit)
+    result = qibo_api.get_result(job_id=job_id)
+    print(result)
+
+
+if __name__ == '__main__':
+    print(f'QIBO_ENVIRONMENT={os.environ["QIBO_ENVIRONMENT"]}')
+    user_example()
