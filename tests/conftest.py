d3529e46c7b0e344a13dca6774b50737bb0f8327
iamtxena <iamtxena@gmail.com>
2022-04-09 11:56:57 +0200

    test: :construction: added base test to mock a connection

---
+++ tests/conftest.py
@@ -0,0 +1,46 @@
+""" Pytest configuration fixtures for each session """
+
+from dataclasses import asdict
+from unittest.mock import patch
+
+import pytest
+
+from qiboconnection.api import API
+from qiboconnection.typings.connection import (
+    ConnectionConfiguration,
+    ConnectionEstablished,
+)
+
+
+@pytest.fixture(scope="session", name="mocked_connection_configuration")
+def fixture_create_mocked_connection_configuration() -> ConnectionConfiguration:
+    """Create a mock connection configuration"""
+    return ConnectionConfiguration(user_id=666, username="mocked_user", api_key="betterNOTaskMockedAPIKey")
+
+
+@pytest.fixture(scope="session", name="mocked_connection_established")
+def fixture_create_mocked_connection_established(
+    mocked_connection_configuration: ConnectionConfiguration,
+) -> ConnectionEstablished:
+    """Create a mock connection configuration"""
+    return ConnectionEstablished(
+        **asdict(mocked_connection_configuration),
+        authorisation_access_token="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3O"
+        + "DkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c",
+        api_path="/api/v1",
+    )
+
+
+@pytest.fixture(scope="session", name="mocked_api")
+def fixture_create_mocked_api_connection(mocked_connection_established: ConnectionEstablished) -> API:
+    """Create a mocked api connection
+
+    Returns:
+        API: API mocked connection
+    """
+    with patch(
+        "qiboconnection.connection.load_config_file_to_disk", return_value=mocked_connection_established
+    ) as mock_config:
+        api = API()
+        mock_config.assert_called()
+        return api
