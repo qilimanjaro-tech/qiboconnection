4bc381b0b6a469f9b078bc4cc0ffa2121691e435
javi <javier.sabariego@qilimanjaro.tech>
2022-05-10 09:35:27 +0200

    ci(api): execute
    Disjointed JobRequest and JobResponse. Done a base class to inherit the common attributes

--- src/qiboconnection/typings/job.py
+++ src/qiboconnection/typings/job.py
@@ -37,28 +37,42 @@ class JobStatus(enum.Enum):
 
 
 @dataclass
-class JobRequest(ABC):
+class JobRESTBase(ABC):
+    """
+    BaseClass for building JobRequest and JobResponse
+    Attributes:
+        user_id (int): User identifier
+        device_id (int): Device identifier
+        description (str): Description of the job
+    """
+
+    user_id: int
+    device_id: int
+    description: str
+
+
+@dataclass
+class JobRequest(JobRESTBase):
     """Job Request
 
     Attributes:
         user_id (int): User identifier
         device_id (int): Device identifier
-        number_shots (int): number of times the job is to be executed
         description (str): Description of the job
-
+        number_shots (int): number of times the job is to be executed
     """
 
-    user_id: int
-    device_id: int
     number_shots: int
-    description: str
 
 
 @dataclass
-class JobResponse(JobRequest):
+class JobResponse(JobRESTBase):
     """Job Response
 
     Attributes:
+        user_id (int): User identifier
+        device_id (int): Device identifier
+        description (str): Description of the job
         job_id (int): Job identifier
         queue_position (int): Job queue position
         status (str | JobStatus): Status of the job
