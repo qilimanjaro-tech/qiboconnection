6e7bf407e0e63fa8d28cbd072ccb8729711c2b94
javi <javier.sabariego@qilimanjaro.tech>
2022-07-04 12:41:56 +0200

    ci(test): utils
    Added test for saving and loading back configuration

--- tests/test_util.py
+++ tests/test_util.py
@@ -1,7 +1,33 @@
 """ Tests methods for util functions """
 import json
 
-from qiboconnection.util import base64url_decode, base64url_encode
+import pytest
+from requests.models import Response
+
+from qiboconnection.connection import ConnectionEstablished
+from qiboconnection.util import (
+    base64url_decode,
+    base64url_encode,
+    load_config_file_to_disk,
+    process_response,
+    write_config_file_to_disk,
+)
+
+
+@pytest.fixture(name="response")
+def fixture_response() -> Response:
+    response = Response()
+    response.status_code = 200
+    response.url = "server/api"
+    response._content = json.dumps({"DEMO": "200"}).encode("utf8")
+    return response
+
+
+@pytest.fixture(name="connection_established")
+def fixture_connection_established() -> ConnectionEstablished:
+    return ConnectionEstablished(
+        api_key="DEMO_KEY", api_path="DEMO_PATH", authorisation_access_token="DEMO_TOKEN", username="DEMO_USERNAME"
+    )
 
 
 def test_base64url_encode():
@@ -42,3 +68,17 @@ def test_base64url_decode_list():
         }
     ]
     assert base64url_decode(data) == expected_decoded
+
+
+def test_save_and_load_config_to_disk(connection_established: ConnectionEstablished):
+    write_config_file_to_disk(config_data=connection_established)
+    recovered_config_data = load_config_file_to_disk()
+    assert connection_established == recovered_config_data
+
+
+def test_process_response(response: Response):
+
+    processed_response = process_response(response=response)
+
+    assert processed_response[0] == json.loads(response.text)
+    assert processed_response[1] == response.status_code
