304985deb756021b14b9e5f9b607bf53355cd9cf
javi <javier.sabariego@qilimanjaro.tech>
2022-07-13 11:11:09 +0200

    ci(tests): connection
    Added mocked connection instead of normal connection for avoiding writes to disk.

--- tests/test_connection.py
+++ tests/test_connection.py
@@ -1,26 +1,8 @@
 """ Test methods for Connection """
 
-import pytest
-
-from qiboconnection.connection import Connection, ConnectionConfiguration
+from qiboconnection.connection import Connection
 
 
-@pytest.fixture(name="wrong_connection_configuration")
-def fixture_wrong_connection_configuration():
-    """Creates a login info object with wrong credentials
-
-    Returns:
-       ConnectionConfiguration: object containing wrong login data."""
-    return ConnectionConfiguration(username="wrong_username", api_key="wrong_key")
-
-
-def test_connection_wrong_login(wrong_connection_configuration: ConnectionConfiguration):
-    """Tries to instance a Connection object with wrong login data expecting to get a ValueError.
-
-    Args:
-        wrong_connection_configuration: login credentials
-    """
-    with pytest.raises(ValueError) as e_info:
-        Connection(configuration=wrong_connection_configuration, api_path="/api/v1")
-
-    assert "Authorisation request failed:" in e_info.value.args[0]
+def test_connection_constructor(mocked_connection: Connection):
+    """Test Connection class constructor"""
+    assert isinstance(mocked_connection, Connection)
