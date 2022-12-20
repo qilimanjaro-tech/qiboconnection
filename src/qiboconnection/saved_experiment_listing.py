694b09818e4b13ddcd5ef587cf93212f95c13062
javi <javier.sabariego@qilimanjaro.tech>
2022-12-20 15:41:08 +0100

    fix(api): ensure dates are properly read
    ensures that when the listing's dataframe reads the dates, they are read as datetimes and not as strings

--- src/qiboconnection/saved_experiment_listing.py
+++ src/qiboconnection/saved_experiment_listing.py
@@ -47,7 +47,16 @@ class SavedExperimentListing(ABC):
     _dataframe: pd.DataFrame = field(init=False)
 
     def __post_init__(self):
-        self._dataframe = pd.DataFrame((asdict(item) for item in self.items))
+        self._dataframe = self._build_dataframe()
+        self._coerce_dataframe_column_types()
+
+    def _build_dataframe(self):
+        """Builds the dataframe from the info in each listing item"""
+        return pd.DataFrame((asdict(item) for item in self.items))
+
+    def _coerce_dataframe_column_types(self):
+        """Parses each column that can potentially have its type missinterpretated to the one we would expect"""
+        self._dataframe["created_at"] = pd.to_datetime(self._dataframe["created_at"])
 
     @classmethod
     def from_response(cls, response_list: List[SavedExperimentListingItemResponse]):
