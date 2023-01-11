80bef529dc048aabbabe43bb0c7f793afb03e2f4
javi <javier.sabariego@qilimanjaro.tech>
2023-01-11 15:30:17 +0100

    feat(tests): added prototyped testing for runcards
    added prototyped testing for runcards. Refactor api's `update_runcard` function

--- tests/data/web_responses/devices.py
+++ tests/data/web_responses/devices.py
@@ -1,63 +1,40 @@
 """ Devices Web Responses """
 
+device_base_response_a = {
+    "device_id": 1,
+    "device_name": "AWS Development Simulator",
+    "status": "available",
+    "channel_id": 2,
+    "number_pending_jobs": 6,
+    "characteristics": {
+        "type": "simulator",
+        "cpu": "Intel @ 4x 2GHz",
+        "gpu": "None",
+        "os": "Ubuntu 22.04 jammy",
+        "kernel": "x86_64 Linux 5.4.0-80-generic",
+        "ram": "4096MiB",
+    },
+}
+
+device_base_response_b = {
+    "device_id": 9,
+    "device_name": "AWS Development Quantum",
+    "status": "available",
+    "channel_id": 2,
+    "characteristics": {"type": "quantum"},
+    "calibration_details": {"t1": 10, "frequency": 988},
+}
+
 
 class Devices:
     """Devices Web Responses"""
 
-    retrieve_response = (
-        {
-            "device_id": 1,
-            "device_name": "AWS Development Simulator",
-            "status": "available",
-            "channel_id": 2,
-            "number_pending_jobs": 6,
-            "characteristics": {
-                "type": "simulator",
-                "cpu": "Intel @ 4x 2GHz",
-                "gpu": "None",
-                "os": "Ubuntu 22.04 jammy",
-                "kernel": "x86_64 Linux 5.4.0-80-generic",
-                "ram": "4096MiB",
-            },
-        },
-        200,
-    )
-
-    update_response = retrieve_response
-
-    retrieve_many_response = (
+    retrieve_response: tuple[dict, int] = (device_base_response_a, 200)
+    update_response: tuple[dict, int] = retrieve_response
+    retrieve_many_response: tuple[tuple[dict, int]] = (
         (
             {
-                "items": [
-                    {
-                        "device_id": 1,
-                        "device_name": "AWS Development Simulator",
-                        "status": "available",
-                        "channel_id": 2,
-                        "characteristics": {
-                            "type": "simulator",
-                            "cpu": "Intel @ 4x 2GHz",
-                            "gpu": "None",
-                            "os": "Ubuntu 22.04 jammy",
-                            "kernel": "x86_64 Linux 5.4.0-80-generic",
-                            "ram": "4096MiB",
-                        },
-                    },
-                    {
-                        "device_id": 9,
-                        "device_name": "AWS Development Quantum",
-                        "status": "available",
-                        "channel_id": 2,
-                        "characteristics": {"type": "quantum"},
-                        "calibration_details": {"t1": 10, "frequency": 988},
-                    },
-                    {
-                        "device_id": 10,
-                        "device_name": "Galadriel Qblox rack                              ",
-                        "status": "offline",
-                        "channel_id": 0,
-                    },
-                ],
+                "items": [device_base_response_a, device_base_response_b],
                 "total": 3,
                 "per_page": 5,
                 "self": "https://qilimanjarodev.ddns.net:8080/api/v1/devices?page=1&per_page=5",
--- tests/data/web_responses/runcards.py
+++ tests/data/web_responses/runcards.py
@@ -1,15 +1,38 @@
 """ Runcards Web Responses """
 
+runcard_base_response = {
+    "runcard_id": 1,
+    "name": "MyDemoRuncard",
+    "user_id": 1,
+    "device_id": 1,
+    "description": "A test runcard",
+    "runcard": "",
+    "created_at": "Fri, 16 Dec 2022 18:40:24 GMT",
+    "qililab_version": "0.0.0",
+}
+
 
 class Runcards:
     """Runcards Web Responses"""
 
-    create_response: tuple[dict, int] = ({}, 201)
-
-    retrieve_many_response: tuple[dict, int] = ({}, 200)
-
-    retrieve_response: tuple[dict, int] = ({}, 200)
-
-    update_response: tuple[dict, int] = ({}, 200)
-
+    create_response: tuple[dict, int] = (runcard_base_response, 201)
+    retrieve_response: tuple[dict, int] = (runcard_base_response, 200)
+    retrieve_many_response: tuple[tuple[dict, int]] = (
+        (
+            {
+                "items": [runcard_base_response],
+                "total": 1,
+                "per_page": 5,
+                "self": "https://qilimanjarodev.ddns.net:8080/api/v1/runcards?page=1&per_page=5",
+                "links": {
+                    "first": "https://qilimanjarodev.ddns.net:8080/api/v1/runcards?page=1&per_page=5",
+                    "prev": "https://qilimanjarodev.ddns.net:8080/api/v1/runcards?page=None&per_page=5",
+                    "next": "https://qilimanjarodev.ddns.net:8080/api/v1/runcards?page=None&per_page=5",
+                    "last": "https://qilimanjarodev.ddns.net:8080/api/v1/runcards?page=1&per_page=5",
+                },
+            },
+            200,
+        ),
+    )
+    update_response: tuple[dict, int] = retrieve_response
     delete_response: tuple[str, int] = ("Runcard deleted", 204)
