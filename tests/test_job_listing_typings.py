7b5404e1b791b1c114a8cb35b2a62d7087e55da8
adria <adria.blanco@qilimanjaro.tech>
2023-06-09 09:03:19 +0200

    test: job_listing typings

--- tests/test_job_listing_typings.py
+++ tests/test_job_listing_typings.py
@@ -1,7 +1,9 @@
-from qiboconnection.typings.job import JobFullData
+from qiboconnection.typings.job import JobFullData, ListingJobResponse
 
 
 def test_JobFullData_typing():
+    """Test JobFullData typing, this is what is returned to the user with get_job()."""
+
     job_full_data = JobFullData(
         queue_position=32,
         status="89",
@@ -13,3 +15,13 @@ def test_JobFullData_typing():
         result={},
     )
     assert isinstance(job_full_data, JobFullData)
+
+
+def test_ListingJobResponse_typing():
+    """Test ListingJobResponse instantation"""
+
+    listing_job_response = ListingJobResponse(
+        status="89", user_id=None, device_id=3, job_type="jaiof", number_shots=84, id=3
+    )
+
+    assert isinstance(listing_job_response, ListingJobResponse)
