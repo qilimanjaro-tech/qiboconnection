aa0a7cab4e2567fd403fa4160c8e672ee4e7edfa
JavierSab <xavin4er96@gmail.com>
2024-10-11 16:35:34 +0200

    feat(linters): introduced ruff, updated deps and delete old hooks

--- setup.py
+++ setup.py
@@ -1,3 +1,17 @@
+# Copyright 2023 Qilimanjaro Quantum Tech
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # Installation script for python
 import os
 import re
@@ -12,7 +26,7 @@ def get_version():
     """Gets the version from the package's __init__ file
     if there is some problem, let it happily fail"""
     VERSIONFILE = os.path.join("src", PACKAGE, "__init__.py")
-    initfile_lines = open(VERSIONFILE, "rt").readlines()
+    initfile_lines = open(VERSIONFILE, "rt", encoding="utf-8").readlines()
     VSRE = r"^__version__ = ['\"]([^'\"]*)['\"]"
     for line in initfile_lines:
         mo = re.search(VSRE, line, re.M)
@@ -21,7 +35,7 @@ def get_version():
 
 
 # Read in requirements
-requirements = open("requirements.txt").readlines()
+requirements = open("requirements.txt", encoding="utf-8").readlines()
 requirements = [r.strip() for r in requirements]
 
 
